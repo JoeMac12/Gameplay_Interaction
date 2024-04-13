2beefdcc479ceaa729b314e1f545233068293bff
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-04-13 05:13:09 -0300

    add credits

---
+++ Assets/AllCredits.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Assets used in this project
+
+/*
+Pixel Art Platformer - Village Props        https://assetstore.unity.com/packages/2d/environments/pixel-art-platformer-village-props-166114
+Enemy Galore 1 - Pixel Art  Art Platformer  https://assetstore.unity.com/packages/2d/characters/enemy-galore-1-pixel-art-208921
+2D Pixel Item Asset Pack        https://assetstore.unity.com/packages/2d/gui/icons/2d-pixel-item-asset-pack-99645
+Tiny RPG - Forest       https://assetstore.unity.com/packages/2d/characters/tiny-rpg-forest-114685
+Pixel Art Key Pack - Animated   https://assetstore.unity.com/packages/2d/pixel-art-key-pack-animated-271986
+Free Wall tile set      https://assetstore.unity.com/packages/2d/environments/free-wall-tile-set-130514
+*/
